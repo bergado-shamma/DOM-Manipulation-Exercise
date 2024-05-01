5fa967e035d06fa48d64329757091ce40c9a8721
bergado_shamma <156270487+bergado-shamma@users.noreply.github.com>
2024-05-01 22:23:17 +0800

    add my files

---
+++ Bergado_Shamma/script.js
@@ -0,0 +1,14 @@
+function addItems() {
+  let newItems = document.querySelector("#new_items");
+  let displayItems = document.querySelector("#display_items");
+
+  let newItemsText = newItems.value.trim();
+  if (newItemsText !== "") {
+    let li = document.createElement("li");
+    li.textContent = newItemsText;
+    displayItems.appendChild(li);
+    newItems.value = "";
+  } else {
+    alert("Enter an item to proceed");
+  }
+}
